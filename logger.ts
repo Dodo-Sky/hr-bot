cb8466ea102ce326f225ee9253f94d4ac7bf9e8b
Eldos <eldos.baktybekov@gmail.com>
2025-06-14 18:37:00 +0600

    Упрощено определение логгера, убрав условие для dev/prod окружений и оставив только конфигурацию для pino-pretty.

--- logger.ts
+++ logger.ts
@@ -8,27 +8,20 @@ if (!fs.existsSync(logDir)) {
     fs.mkdirSync(logDir, { recursive: true })
 }
 
-// 2. Определяем, dev или prod
-const isDev = process.env.NODE_ENV !== 'production'
-
 // 3. Логгер
 export const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
     transport: {
         targets: [
-            ...(isDev
-                ? [
-                    {
-                        target: 'pino-pretty',
-                        options: {
-                            colorize: true,
-                            translateTime: 'HH:MM:ss',
-                            ignore: 'pid,hostname'
-                        },
-                        level: 'debug'
-                    }
-                ]
-                : []),
+            {
+                target: 'pino-pretty',
+                options: {
+                    colorize: true,
+                    translateTime: 'HH:MM:ss',
+                    ignore: 'pid,hostname'
+                },
+                level: 'debug'
+            },
             {
                 target: 'pino/file',
                 options: { destination: path.join(logDir, 'app.log') },
